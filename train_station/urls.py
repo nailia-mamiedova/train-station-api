d7fb686eb68050e1f95a6fac67d826f2bc6d630a
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-14 13:46:45 +0300

    Install and configure django debug toolbar

--- train_station/urls.py
+++ train_station/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/station/", include("station.urls", namespace="station")),
     path("api/user/", include("user.urls", namespace="user")),
+
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
