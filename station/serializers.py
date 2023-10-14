74f8951e98d07cbae9e442c31e14bdb80168f3fc
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-14 11:51:38 +0300

    Add SlugRelatedField for all related fields in serializers

--- station/serializers.py
+++ station/serializers.py
@@ -19,6 +19,10 @@ class TrainTypeSerializer(serializers.ModelSerializer):
 
 
 class TrainSerializer(serializers.ModelSerializer):
+    train_type = serializers.SlugRelatedField(
+        slug_field="name", queryset=TrainType.objects.all()
+    )
+
     class Meta:
         model = Train
         fields = ("id", "name", "cargo_num", "places_in_cargo", "train_type")
@@ -31,6 +35,13 @@ class StationSerializer(serializers.ModelSerializer):
 
 
 class RouteSerializer(serializers.ModelSerializer):
+    source = serializers.SlugRelatedField(
+        slug_field="name", queryset=Station.objects.all()
+    )
+    destination = serializers.SlugRelatedField(
+        slug_field="name", queryset=Station.objects.all()
+    )
+
     class Meta:
         model = Route
         fields = ("id", "source", "destination", "distance")
@@ -43,6 +54,13 @@ class CrewSerializer(serializers.ModelSerializer):
 
 
 class TripSerializer(serializers.ModelSerializer):
+    route = serializers.StringRelatedField(
+        many=False, read_only=True
+    )
+    train = serializers.SlugRelatedField(
+        slug_field="name", queryset=Train.objects.all()
+    )
+
     class Meta:
         model = Trip
         fields = ("id", "route", "train", "departure_time", "arrival_time")
@@ -55,6 +73,13 @@ class OrderSerializer(serializers.ModelSerializer):
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    trip = serializers.SlugRelatedField(
+        slug_field="id", queryset=Trip.objects.all()
+    )
+    order = serializers.SlugRelatedField(
+        slug_field="id", queryset=Order.objects.all()
+    )
+
     class Meta:
         model = Ticket
         fields = ("id", "cargo", "seat", "trip", "order")
