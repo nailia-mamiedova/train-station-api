2732e188633f2e25c7d0faa9d83b3696045d687f
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-14 11:53:36 +0300

    Add routes for all views in app urls

--- station/urls.py
+++ station/urls.py
@@ -1,7 +1,30 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from station.views import (
+    TrainTypesViewSet,
+    TrainViewSet,
+    StationViewSet,
+    RouteViewSet,
+    CrewViewSet,
+    TripViewSet,
+    OrderViewSet,
+    TicketViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("train_types", TrainTypesViewSet)
+router.register("trains", TrainViewSet)
+router.register("stations", StationViewSet)
+router.register("routes", RouteViewSet)
+router.register("crews", CrewViewSet)
+router.register("trips", TripViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
+
 
 urlpatterns = [
-
+    path("", include(router.urls)),
 ]
 
 app_name = "station"
