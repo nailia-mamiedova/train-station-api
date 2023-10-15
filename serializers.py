a959e98025291fd0eecfdcc5e9fbbd17ae162a26
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-15 13:21:56 +0300

    Delete an accidentally created serializer outside the station app

--- serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from station.models import TrainType
-
-
-class TrainTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TrainType
-        fields = ("id", "name")
