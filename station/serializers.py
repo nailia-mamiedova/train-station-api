d2963fd9b2dca313be38463dc39c14f14338f7bd
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-17 13:51:52 +0300

    Add validate method for ticket serializer

--- station/serializers.py
+++ station/serializers.py
@@ -95,6 +95,13 @@ class TripDetailSerializer(TripSerializer):
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(TicketSerializer, self).validate(attrs=attrs)
+        Ticket.validate_cargo(attrs["cargo"], attrs["trip"].train.cargo_num, serializers.ValidationError)
+        Ticket.validate_seat(attrs["seat"], attrs["trip"].train.places_in_cargo, serializers.ValidationError)
+
+        return data
+
     class Meta:
         model = Ticket
         fields = ("id", "cargo", "seat", "trip", "order")
